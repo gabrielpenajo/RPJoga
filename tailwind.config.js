93203bb74f34d0bfbb3c0b6127cc62350f79c573
gabrielpenajo <gabrielpenajo13@gmail.com>
2023-01-16 09:35:18 -0300

    Config tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'main': '#682d63',
+        'secondary': '#7576bd',
+        'success': '#5fb49c',
+        'warning': '#ffff70',
+        'error': '#e6614c',
+        'neutral': '#2c2c2c',
+        'white': '#fff',
+        'black': '#000',
+      },
+    },
+  },
+  plugins: [],
+}
